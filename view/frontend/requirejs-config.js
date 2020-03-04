fba1924a2e029a32f61d4676e28bd37f055d99a0
Kiel Pykett <40261741+pykettk@users.noreply.github.com>
2020-03-04 23:40:35 +0000

    Create requirejs-config.js

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    "map": {
+        "*": {
+            "Magento_Checkout/template/summary/cart-items.html": "PykettK_ExpandedOrderSummary/template/summary/cart-items.html"
+        }
+    }
+};
